8d3535da9f8855e8a1320f32df89bfe83c39dae0
Md. Rakib Hasan <rhsrakib030@gmail.com>
2024-02-28 19:28:40 +0600

    This is a chatting application. For run this, go to your command prompt and execute node server.js

---
+++ Chatting_Asynchronous/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const SocketIOFileUpload = require('socketio-file-upload');
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const { v4: uuidv4 } = require('uuid');
+const { Socket } = require('net');
+var users = [];
+app
+	.use(SocketIOFileUpload.router)
+	.use(express.static(__dirname));
+var t = "";
+var ofn = "";
+io.on('connection', socket => {
+	console.log('New user connected');
+	var uploader = new SocketIOFileUpload();
+	uploader.dir = "./uploads";
+	uploader.listen(socket);
+	socket.on('register', name => {
+		socket.username = name;
+		users.push(name);
+		socket.emit('joined', socket.username);
+		io.emit('userlist', users);
+	});
+	socket.on('chat', msg => {
+		socket.broadcast.emit('message', { from: socket.username, msg: msg })
+	});
+	uploader.on("start", event => {
+		var old_name = event.file.name
+		ofn = old_name;
+		var arr = old_name.split('.');
+		var new_name = uuidv4() + '.' + arr[arr.length - 1];
+		if (['png', 'jpg', 'jpeg', 'gif', 'svg'].indexOf(arr[arr.length - 1]) >= 0) {
+			t = 'image';
+		}
+		else {
+			t = 'file';
+		}
+		console.log(t);
+		return event.file.name = new_name;
+	});
+	uploader.on("saved", function (event) {
+		io.emit('uploaded', { from: socket.username, file: event.file.name, type: t, ofn: ofn });
+	});
+	uploader.on("error", function (event) {
+		console.log("Error from uploader", event);
+	});
+});
+server.listen(8035, () => {
+	console.log('listening on http://localhost:8035');
+});
+
